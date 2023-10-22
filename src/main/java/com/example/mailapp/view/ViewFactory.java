fb6a5328d2912d1a866397c3c07e0d2db7467b2a
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-10-22 18:57:42 -0400

    initializing stages - added implementation of showLoginWindow in ViewFactory

--- src/main/java/com/example/mailapp/view/ViewFactory.java
+++ src/main/java/com/example/mailapp/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.example.mailapp.view;
 
 import com.example.mailapp.EmailManager;
+import com.example.mailapp.controller.BaseController;
+import com.example.mailapp.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -11,6 +19,24 @@ public class ViewFactory {
     }
 
     public void showLoginWindow() {
+
         System.out.println("show login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
