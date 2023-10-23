680a3996aa94a8986b4c3d76415fcbf8f4dd15e3
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2023-10-22 20:14:36 -0400

    stage actions - close previous stage and open new one on button click.

--- src/main/java/com/example/mailapp/controller/LoginWindowController.java
+++ src/main/java/com/example/mailapp/controller/LoginWindowController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 
 public class LoginWindowController extends BaseController {
 
@@ -25,6 +26,9 @@ public class LoginWindowController extends BaseController {
     @FXML
     void loginButtonAction() {
         System.out.println("click!");
+        viewFactory.showMainWindow();
+        Stage stage = (Stage) errorLabel.getScene().getWindow();
+        viewFactory.closeStage(stage);
     }
 
 }
